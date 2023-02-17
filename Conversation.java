718c34a9bdac33edbed88fb44c116d1c959160d6
Yemilly Soto <ysoto@smith.edu>
2023-02-16 23:26:24 -0500

    fixed

--- Conversation.java
+++ Conversation.java
@@ -4,13 +4,14 @@ class Conversation {
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
+  System.out.println("How many rounds???");
 
   Scanner input = new Scanner(System.in);
     int num_rounds = input.nextInt();
-    for (int i = 0; i< num_rounds; i++);
+      for (int i = 0; i< num_rounds; i++);
     input.close();
 
-  System.out.println("How many rounds???");
+  
   
   System.out.println("Hi there! What's on your mind???");
   
