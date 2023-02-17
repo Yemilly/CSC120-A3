61ca0c5996949d31c007b6f4ac44a73668e6c06c
Yemilly Soto <ysoto@smith.edu>
2023-02-16 23:25:09 -0500

    debugging

--- Conversation.java
+++ Conversation.java
@@ -6,13 +6,17 @@ class Conversation {
     // You will start the conversation here.
 
   Scanner input = new Scanner(System.in);
-  int num_rounds = input.nextInt();
+    int num_rounds = input.nextInt();
     for (int i = 0; i< num_rounds; i++);
-  
+    input.close();
+
   System.out.println("How many rounds???");
   
   System.out.println("Hi there! What's on your mind???");
   
   
 
-}
+
+  } 
+
+} 
