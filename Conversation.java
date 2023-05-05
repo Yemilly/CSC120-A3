93b38ef0737e735d94e654ee672534d3deaa445a
Yemilly Soto <ysoto@smith.edu>
2023-05-04 22:18:13 -0400

    final

--- Conversation.java
+++ Conversation.java
@@ -1,26 +1,83 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 class Conversation {
 
   public static void main(String[] arguments) {
-    // You will start the conversation here.
-  System.out.println("How many rounds???");
 
-  Scanner input = new Scanner(System.in);
-    int num_rounds = input.nextInt();
-      for (int i = 0; i< num_rounds; i++);
+    Scanner input = new Scanner(System.in);
+    ArrayList<String> transcript = new ArrayList<String>();
+    transcript.add("TRANSCRIPT: \n ");
+   
+    transcript.add("How many rounds? \n ");
+   System.out.println("How many rounds?");
+
+    int rounds = input.nextInt();
+
+    String number = Integer.toString(rounds);
+    transcript.add(number);
+  
+    Scanner speech = new Scanner(System.in);
+    transcript.add("Hi there! What's on your mind??? \n ");
+    System.out.println("Hi there! What's on your mind???");
+
+  
+
+    for (int i=0; i<rounds; i++){
+      String line = speech.nextLine();
+      transcript.add(line);
+      transcript.add("\n");
+
+      String[] person = {"I","me","am","you","my","your" };
+      String [] mirror = {"you", "you","are", "I", "your", "my"};
+
+      boolean x=false;
+
+      String[] newline = line.split(" ",0);
+
+        for(int j=0; j<newline.length; j++){
+          for (int k=0; k<person.length; k++){
+        
+          if (newline[j].equals(person[k])){
+           
+            newline[j]=mirror[k];
+            x=true;
+          
+            break;
+          }
+          }
+       
+      }
+
+      if (x){
+        System.out.println(String.join(" ", newline) + "?");
+      } 
+      
+      else {
+        String[] responses = {"Ohh Okay", "How interesting!", "Really?", "Mmm-hm", "Oh I see!" };
+
+        Random ran = new Random();
+        int nxt = ran.nextInt(5);
+
+        System.out.println(responses[nxt]);
+        transcript.add(responses[nxt]);
+        transcript.add("\n");
+      }
+
+
+    } 
+    
+    System.out.println("Thanks for chatting!"); 
+    transcript.add("Thanks for chatting!");
+    transcript.add("\n");
+    
+    for (int i=0; i<transcript.size(); i++){
+      System.out.print(transcript.get(i) );
+    }
+
     input.close();
-
-
-
-  System.out.println("Hi there! What's on your mind???");
-  
-
-
-
-  System.out.print("Okay, bye!");
-  
-  
-  } 
-
-} 
+    speech.close();
+  }
+}
--- reflection.md
+++ reflection.md
@@ -1,9 +1,15 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+i didnt really work with any one but i went to TA hours for help
+
 - Which session(s) of TA / office hours did you attend?
+On wednesdays
 - What are your initial impressions of Java? 
 I find Java to be alot more complicated than python.
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+Python was much more simpler for me than java, but still gettign the hang of it.
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
 I tried working throught the warm-up for this hw but i still founf it to be pretty challenging. Future advise is most def ask for help early on!!!
